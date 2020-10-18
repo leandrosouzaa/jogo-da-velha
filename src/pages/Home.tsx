4595d8588e97f1427dcac9217542dff90e11406b
leandrosouzaa <leandror.dev@gmail.com>
2020-10-18 13:37:08 -0300

    feat:  show squares values

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Square} from '../components';
+import { useGame } from '../hooks/game';
 
 const Home: React.FC = () => {
+   const { squares } = useGame();
+   
    return (
       <main className="container">
-         <h1>Tic Tac Toe</h1>
+         <div className="game-container">
+            <div className="squares-container">
+               {squares.map((square, index) => (
+                  <Square key={index} value={square} index={index} />
+               ))}
+            </div>
+         </div>
       </main>
    );
 }
