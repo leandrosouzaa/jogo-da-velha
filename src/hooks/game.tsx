fb541c00dbc27254c0acb2c94f188018f9d293a3
leandrosouzaa <leandror.dev@gmail.com>
2020-10-18 15:29:32 -0300

    feat: win validate

--- src/hooks/game.tsx
+++ src/hooks/game.tsx
@@ -1,18 +1,52 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import checkWinner from '../utils/checkWinner'
 
 interface GameContextData {
    squares: string[];
    setSquares(newSquares: string[]): void;
+   isXNext: boolean;
+   setIsXNext(value: boolean): void;
+   isBlocked: boolean
+}
+
+interface PointsProps {
+   x: number;
+   o: number;
 }
 
 const GameContext = createContext({} as GameContextData);
 
 const GameProvider: React.FC = ({children}) => {
-   const [squares, setSquares] = useState<string[]>(() => Array(9).fill(''));
+   const [squares, setSquares] = useState<string[]>(Array(9).fill(''));
+   const [isXNext, setIsXNext] = useState(true);
+   const [points, setPoints] = useState<PointsProps>({o: 0, x: 0});
+   const [lastWinner, setLastWinner] = useState('');
+   const [isBlocked, setIsBlocked] = useState(false)
+
+   useEffect(() => {
+      const winner: string = checkWinner(squares); 
+      
+      if (!winner)
+         return;
+      
+      setLastWinner(winner);
+      setPoints({...points, [winner.toLowerCase()]: points[winner.toLowerCase()] + 1});
+      setIsBlocked(true);
+      alert(`O Jogador ${winner} ganhou! Parabéns!!`);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [squares])
+
+   const valuesToProvider = {
+      squares,
+      setSquares,
+      isXNext, 
+      setIsXNext,
+      isBlocked
+   }
 
    return (
       <GameContext.Provider
-         value={{squares, setSquares}}
+         value={valuesToProvider}
       >
          {children}
       </GameContext.Provider>
@@ -23,7 +57,7 @@ function useGame(): GameContextData {
    const context = useContext(GameContext);
 
    if (!context) 
-      throw new Error('useGame must be used within an AuthProvider');
+      throw new Error('useGame must be used within an GameProvider');
 
    return context;
 }
