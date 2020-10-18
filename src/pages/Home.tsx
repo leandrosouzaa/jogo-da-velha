ca6709f4e9d95529a93d2b520f20e8bc6e274051
leandrosouzaa <leandror.dev@gmail.com>
2020-10-18 11:32:22 -0300

    feat: init home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+   return (
+      <main className="container">
+         <h1>Tic Tac Toe</h1>
+      </main>
+   );
+}
+
+export default Home;
